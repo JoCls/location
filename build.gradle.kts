b1b220b126f36bd372b5265d0989a388aefd042f
JoCls <jordanclose66@gmail.com>
2024-07-15 22:22:00 +0200

    001_Initialize_Project : Added build.gradle config for spring, lombok, junit, ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.6.10"
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "be.jocls"
@@ -10,6 +12,12 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.28")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "locatio"
+rootProject.name = "location"
 
