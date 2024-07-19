277842e2f9d8a757203c161abf0a28af83c25467
JoCls <jordanclose66@gmail.com>
2024-07-19 10:53:13 +0200

    004 : Updated User model to add new fields

--- src/main/java/be/jocls/domain/model/User.java
+++ src/main/java/be/jocls/domain/model/User.java
@@ -13,13 +13,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-
     private String password;
-
     private String email;
-
+    private String firstName;
+    private String lastName;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
