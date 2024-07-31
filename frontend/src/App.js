552f1af2cbf69115e3416b3ec15637f4ef52a0f2
JoCls <jordanclose66@gmail.com>
2024-07-31 21:20:29 +0200

    027_Protected_Route : Added ProtectedRoute logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MenuPage from './pages/MenuPage';
 import ReservationsPage from './pages/ReservationsPage'; 
 import CreateReservationPage from './pages/CreateReservationPage'; 
 import AdminPanelPage from './pages/AdminPanelPage';
-import { AuthProvider } from './context/AuthContext';
+import ReserveEquipmentPage from './pages/ReserveEquipmentPage';
+import ReserveClassroomPage from './pages/ReserveClassroomPage';
 
 function App() {
-  const isAuthenticated = true; 
-  const userRole = "ADMIN"; 
   return (
     <AuthProvider>
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/menu" element={<MenuPage />} />
+
+        {/* Protect the following routes */}
+        <Route path="/menu" element={<ProtectedRoute><MenuPage /></ProtectedRoute>}/>
+        <Route path="/reservations" element={<ProtectedRoute><ReservationsPage /></ProtectedRoute>}/>
+        <Route path="/create-reservation" element={<ProtectedRoute><CreateReservationPage /></ProtectedRoute>}/>
+        <Route path="/reserve-equipment" element={<ProtectedRoute><ReserveEquipmentPage /></ProtectedRoute>}/>
+        <Route path="/reserve-classroom" element={<ProtectedRoute><ReserveClassroomPage /></ProtectedRoute>}/>
+        <Route path="/admin" element={<ProtectedRoute><AdminPanelPage /></ProtectedRoute>}/>
+
+        {/* Fallback route: Redirects to login if the path doesn't match */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   </AuthProvider>
---
+++ frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,22 @@
+// src/components/ProtectedRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+// ProtectedRoute component
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated } = useAuth();
+
+    console.log('isAuthenticated ProtectedRoute :', isAuthenticated);
+
+
+  // If not authenticated, redirect to login page
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // If authenticated, render the children components
+  return children;
+};
+
+export default ProtectedRoute;
